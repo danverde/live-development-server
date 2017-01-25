1b85bd1ec2f9e5cce87179dcd72549b0e9978084
danverde <gre12014@byui.edu>
2017-01-25 12:36:10 -0700

    Added feedback & optional port parameter
    Added feedback on run including a success message, the port being used, and a script tag to reference the server from the web. Port is an optional 2nd parameter with 8000 as default. The file is being exported so it can be used as an npm package.

--- launch.js
+++ launch.js
@@ -1,22 +1,41 @@
 var fs = require("fs");
 var https = require("https");
 var url = require("url");
+var port = getPort();
+
+
+function getPort() {
+    if (process.argv[2] == undefined)
+        return 8000;
+    else
+        return process.argv[2];
+}
+
+function getScript(port) {
+    var url = 'https://localhost:' + port + '/fileDirectory';
+    return "<script scr=" + url + "></script>";
+}
 
 function processRequest(req, res) {
     var request = url.parse(req.url).pathname;
     var reader = fs.createReadStream("." + request);
     reader.pipe(res);
     console.log(request);
-    reader.on("error", () => {
-        res.writeHead(200);
+    reader.on("error", function () {
+        res.writeHead(404);
         res.write("404:\nThe page you are looking for does not exist...\nSO GET LOST!\neven though you already are...");
         res.end();
     });
 }
 
-var options ={
-    key:fs.readFileSync("./keys/server.key"),
-    cert:fs.readFileSync("./keys/server.crt")
+var options = {
+    key: fs.readFileSync(__dirname + "/keys/server.key"), // __dirname to find directory
+    cert: fs.readFileSync(__dirname + "/keys/server.crt")
 }
 
-https.createServer(options,processRequest).listen(8000)
+function main() {
+    https.createServer(options, processRequest).listen(port) // port dynamic
+    console.log("Server is active on port " + port);
+    console.log(getScript(port)); // also diaplay the script tag with data...
+}
+exports.server = main();
